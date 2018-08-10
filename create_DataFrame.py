bb9043e0555f999540408d9935c900a41bef7059
Ruperto Arrieta Jr <totoarrieta@yahoo.com>
2018-08-10 10:10:52 +0800

    renamed wa.py

---
+++ create_DataFrame.py
@@ -0,0 +1,6 @@
+import pandas as pd #load the Pandas package
+
+df=pd.read_csv("WA_Sales_Products_2012-14.csv") #read the file into a DataFrame that Pandas supports
+
+
+
--- wa.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-#import numpy as np
-#from scipy.stats import ttest_ind
-
-df=pd.read_csv("WA_Sales_Products_2012-14.csv")
-
-
